4bb3f505d8bd5aa349a5daf91ee49bad6075b7a0
default <default@PC_PORTABLE_55>
2023-06-11 01:12:13 +0200

    MAJ des allocations par un bouton

---
+++ odoo-usability/quickparts_base/__manifest__.py
@@ -0,0 +1,29 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Quickparts Base',
+    'version': '14.0.1.0.3',
+    'category': '',
+    'license': 'AGPL-3',
+    'summary': 'Usability improvements for quickparts',
+    'description': """
+Quickparts usability
+=================
+
+This module provides several small usability improvements for percipio:
+
+* TODO: update this list
+
+This module has been written by Honoré Magnin-Feysot from ExNihiloSolution <https://github.com/hmagninfeysot>.
+    """,
+    'author': 'Honoré Magnin-Feysot',
+    'website': 'http://odoo.exnihilosolution.com',
+    'depends': [
+        'base',
+        ],
+    'data': [
+       # 'data/helpdesk_data.xml',
+       # 'views/quickupdate_quickanalyse_des_presences.xml',
+        ],
+    'installable': True,
+}
---
+++ odoo-usability/quickparts_base/models/x_attendance_analysis.py
@@ -0,0 +1,33 @@
+from odoo import api, fields, models, _
+from datetime import datetime
+
+class attendance_analysis(models.Model):
+    _name = "attendance_analysis"
+    _description ="Analyse des absences"
+ 
+    name = fields.Char(string='Nom', required=True)
+    date = fields.Date(string='Date')
+    employee_id = fields.Many2one('hr.employee', string='Employé')
+    hours_attendance = fields.Float(string='Présence')
+    hours_leave = fields.Float(string='Absence')
+    hours_public_holiday = fields.Float(string='Jour férier')
+    hours_theoretical = fields.Float(string='Théorique')
+    gap = fields.Float(string='Gap')
+
+    def action_recalculate_allocation(self):
+        self.ensure_one()
+        allocations = self.env['hr.leave.allocation'].search([('name', '=', 'Allocation automatique timbrage'),('x_name_timbrage', '=', self.name)])
+        allocations.write({'state': 'draft'})
+        allocations.unlink()
+        hours_theo = self.hours_theoretical
+        nb_days = round((self.gap/hours_theo),2)
+        self.env['hr.leave.allocation'].create({
+            'holiday_type' : 'employee',
+            'employee_id': self.employee_id.id,
+            'name' : 'Allocation automatique timbrage',
+            'holiday_status_id': 6,
+            'allocation_type' : 'regular',
+            'number_of_days': nb_days,
+            'state' : 'validate',
+            'x_name_timbrage' : self.name,
+        })
